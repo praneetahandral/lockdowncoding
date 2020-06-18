b32d0e2cf1306234261ae2ef7d86e57cb28389f4
praneetahandral <65345252+praneetahandral@users.noreply.github.com>
2020-06-18 12:51:33 +0530

    Add files via upload

---
+++ MagicNo_18june.cpp
@@ -0,0 +1,21 @@
+ 
+#include <bits/stdc++.h> 
+using namespace std; 
+int nthMagicNo(int n) 
+{ 
+	int pow = 1, answer = 0; 
+	while (n) 
+	{ 
+	pow = pow*5; 
+	if (n & 1) 
+		answer += pow; 
+	n >>= 1; // or n = n/2 
+	} 
+	return answer; 
+} 
+int main() 
+{ 
+	int n = 5; 
+	cout << "nth magic number is " << nthMagicNo(n) << endl; 
+	return 0; 
+} 
